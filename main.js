ea3e0c37e69c21e853f949acfc1dd83510a6c964
EstebanEF <eef251195@gmail.com>
2021-10-10 14:03:41 -0600

    microservice

---
+++ main.js
@@ -0,0 +1,78 @@
+// main.js
+const fs = require('fs')
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+const port = process.env.PORT || 8085
+let revistas = []
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Content-Type', 'application/json');
+    next();
+});
+
+const loadRevistas = () => {
+    fs.readFile(__dirname + '/' + 'revistas.json', 'utf8', (err, data) => {
+        revistas = JSON.parse(data)
+    });
+}
+loadRevistas()
+
+const saveRevistas = () => {
+    let data = JSON.stringify(revistas)
+    fs.writeFileSync(__dirname + '/' + 'revistas.json', data)
+}
+
+app.get('/revista', (req, res) => {
+    res.json(revistas);
+})
+
+app.get('/revista/:id', (req, res) => {
+    let revista = revistas.find(i => i.id == req.params.id);
+    if (revista == undefined)
+        res.status(404).send('Revista not found');
+    else
+        res.json(revista);
+})
+
+app.post('/revista', (req, res) => {
+    let index = revistas.findIndex(i => i.id == req.params.id);
+    if (index != -1)
+      res.status(404).send('Revista already exits'); 
+    else {
+      revistas.push(req.body);
+      saveRevistas();
+    }
+    res.status(200).send('Revista added');
+  })
+
+app.put('/revista/:id', (req, res) => {
+    let index = revistas.findIndex(i => i.id == req.params.id);
+    if (index == -1)
+        res.status(404).send('Revista not found');
+    else {
+        revistas[index] = req.body;
+        saveRevistas();
+    }
+    res.status(200).send('Revista updated');
+})
+
+app.delete('/revista/:id', (req, res) => {
+    let index = revistas.findIndex(i => i.id == req.params.id);
+    if (index == -1)
+        res.status(404).send('Revista not found');
+    else {
+        revistas = revistas.filter(i => i.id != req.params.id);
+        saveRevistas();
+    }
+    res.status(200).send('Revista deleted');
+})
+
+app.listen(port, () => 
+  console.log(`Revistas Server listening on port ${port}`)
+)
